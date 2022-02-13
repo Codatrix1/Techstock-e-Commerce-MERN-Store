03cabfa6af1224e4e3f2739aa2ce99d6de44490d
Codatrix <ankur.codatrix@gmail.com>
2022-02-13 22:24:54 +0530

    React setup

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = function () {
+  return (
+    <React.Fragment>
+      <h1>Welcome to Techstock</h1>
+    </React.Fragment>
+  );
+};
+
+export default App;
