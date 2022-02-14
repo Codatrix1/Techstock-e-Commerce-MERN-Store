033794febd4c7ad28aee54ba5c255135bd5da6c3
Codatrix <ankur.codatrix@gmail.com>
2022-02-14 12:49:50 +0530

    Productscreen Setup with Product Details

--- frontend/src/screens/Productscreen.jsx
+++ frontend/src/screens/Productscreen.jsx
@@ -1,7 +1,85 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 
-const Productscreen = () => {
-  return <div>Product</div>;
+// Components
+import Rating from "../components/Rating";
+
+// static product file
+import products from "../products";
+
+const Productscreen = ({ match }) => {
+  const product = products.find((p) => p._id === match.params.id);
+
+  return (
+    <React.Fragment>
+      {/* Go Back to Home Button*/}
+      <Link className="btn btn-light my-3" to="/">
+        Go Back
+      </Link>
+
+      {/* Single Product Screen*/}
+      <Row>
+        <Col md={5}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={4}>
+          {/* Product info list*/}
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              {/* Rating Component*/}
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} ratings`}
+              />
+            </ListGroup.Item>
+
+            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+            <ListGroup.Item>Description: {product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+
+        <Col md={3}>
+          <Card>
+            {/* Price, stock, add to cart list*/}
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            </ListGroup>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.countInStock > 0 ? "In Stock" : "Out Of Stock"}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  className="btn-block"
+                  type="button"
+                  disabled={product.countInStock === 0}
+                >
+                  Add To Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </React.Fragment>
+  );
 };
 
 export default Productscreen;
