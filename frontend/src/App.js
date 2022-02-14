246ad97e254f343d919ad3cf1fadfd5ac584a240
Codatrix <ankur.codatrix@gmail.com>
2022-02-14 12:02:25 +0530

    Achieved routing with react-router-dom V5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,33 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
+// Components
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Homescreen from "./screens/Homescreen";
 
+// Screens
+import Homescreen from "./screens/Homescreen";
+import Productscreen from "./screens/Productscreen";
+
+// React Component
 const App = function () {
   return (
-    <React.Fragment>
+    <Router>
       {/*Header*/}
       <Header />
 
       {/*Body*/}
       <main className="py-3">
         <Container>
-          <Homescreen />
+          <Route path="/" exact component={Homescreen} />
+          <Route path="/product/:id" component={Productscreen} />
         </Container>
       </main>
 
       {/*Footer*/}
       <Footer />
-    </React.Fragment>
+    </Router>
   );
 };
 
--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,23 +1,26 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
+// Components
 import Rating from "./Rating";
 
+// React component
 const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
       {/* Product Image*/}
-      <a href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant="top" />
-      </a>
+      </Link>
 
       <Card.Body>
         {/* Product Title*/}
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <Card.Title as="div">
             <strong>{product.name}</strong>
           </Card.Title>
-        </a>
+        </Link>
 
         <Card.Text as="div">
           {/* Product Rating & Review Component */}
