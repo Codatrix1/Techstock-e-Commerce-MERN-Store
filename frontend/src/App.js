e6d6f04d20ebb84c3626a3472212d0ba72f29cd2
Codatrix <ankur.codatrix@gmail.com>
2022-02-14 09:16:34 +0530

    React-Bootstrap Setup - Header and Footer Components Basic Setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = function () {
   return (
     <React.Fragment>
-      <h1>Welcome to Techstock</h1>
+      {/*Header*/}
+      <Header />
+
+      {/*Body*/}
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to Techstock</h1>
+        </Container>
+      </main>
+
+      {/*Footer*/}
+      <Footer />
     </React.Fragment>
   );
 };
