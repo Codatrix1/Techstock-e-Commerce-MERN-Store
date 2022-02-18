376b4d98cb5616c694163e7a416f6bb91b8acfd0
Codatrix <ankur.codatrix@gmail.com>
2022-02-18 09:29:04 +0530

    Loader - Message components setup to Homescreen component

--- frontend/src/screens/Homescreen.jsx
+++ frontend/src/screens/Homescreen.jsx
@@ -3,12 +3,16 @@ import { Row, Col } from "react-bootstrap";
 
 // Components
 import Product from "../components/Product";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
 // redux stuff
 import { useDispatch, useSelector } from "react-redux";
 import { listProducts } from "../actions/productActions";
 
-// React Component
+//-------------------------
+// Main React Component
+//--------------------------
 const Homescreen = () => {
   const dispatch = useDispatch();
 
@@ -23,9 +27,9 @@ const Homescreen = () => {
     <React.Fragment>
       <h1>Latest Products</h1>
       {loading ? (
-        <h2>Loading...</h2>
+        <Loader />
       ) : error ? (
-        <h3>{error}</h3>
+        <Message variant="danger">{error}</Message>
       ) : (
         <Row>
           {products.map((product) => (
@@ -40,4 +44,7 @@ const Homescreen = () => {
   );
 };
 
+//-----------------
+// Default export
+//----------------
 export default Homescreen;
